8f64202b587f5abd6c5821ba88daac0f48b19075
unknown <alekseyleha@mail.ru>
2022-06-30 14:29:06 +0400

    delete keys from repo

--- keys.js
+++
@@ -1,1 +0,0 @@
-export const keys = { spoonAPIKey: "2adf7e0ce3d8428f953f022f9543bb6f" };
--- src/firebase_init/fb_init/initialFB.ts
+++ src/firebase_init/fb_init/initialFB.ts
@@ -1,16 +1,8 @@
+import { keys } from "@/../keys";
 import { initializeApp } from "firebase/app";
 import * as FBdatabase from "firebase/database";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDxdPNvL8leleeU8m76kCXcC6hOr3ueNjg",
-  authDomain: "lifeafterdiets-30c24.firebaseapp.com",
-  databaseURL: "https://lifeafterdiets-30c24-default-rtdb.firebaseio.com",
-  projectId: "lifeafterdiets-30c24",
-  storageBucket: "lifeafterdiets-30c24.appspot.com",
-  messagingSenderId: "543791507558",
-  appId: "1:543791507558:web:8aae671c3298a6b16365a7",
-  measurementId: "G-1EXMBFBYM8",
-};
+const firebaseConfig = keys.fbAPIKey;
 
 export const app = initializeApp(firebaseConfig);
 
