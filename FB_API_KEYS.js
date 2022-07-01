b08bee8396c1aec6ff4673f9f7dfe55075ab4c7f
Aleks164 <alekseyleha@mail.ru>
2022-07-01 19:42:26 +0300

    try sith git secret

--- FB_API_KEYS.js
+++ FB_API_KEYS.js
@@ -1,10 +1,10 @@
 export const FB_API_KEYS = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
+    apiKey: "AIzaSyDxdPNvL8leleeU8m76kCXcC6hOr3ueNjg",
+    authDomain: "lifeafterdiets-30c24.firebaseapp.com",
+    databaseURL: "https://lifeafterdiets-30c24-default-rtdb.firebaseio.com",
+    projectId: "lifeafterdiets-30c24",
+    storageBucket: "lifeafterdiets-30c24.appspot.com",
+    messagingSenderId: "543791507558",
+    appId: "1:543791507558:web:8aae671c3298a6b16365a7",
+    measurementId: "G-1EXMBFBYM8",
 };
